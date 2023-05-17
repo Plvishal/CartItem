13daa427fcff52d90bc053d492ebff94755bf0f7
Vishal <vishalpal1501@gmail.com>
2023-05-17 09:53:17 +0530

    Add image link  inside src/App.js and also do some changes inside the CartItem

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class CartItem extends React.Component {
+const CartItem = (props) => {
   // constructor() {
   //   super();
   //   this.state = {
@@ -61,44 +61,44 @@ class CartItem extends React.Component {
   //     };
   //   });
   // };
-  render() {
-    console.log('this.props', this.props);
-    const { price, title, qty } = this.props.product;
-    return (
-      <div className="cart-item">
-        <div className="left-block">
-          <img style={styles.image} alt="" />
-        </div>
-        <div className="right-block">
-          <div style={{ fontSize: 25 }}>{title}</div>
-          <div style={{ color: '#777' }}>Rs {price} </div>
-          <div style={{ color: '#777' }}>Qty: {qty} </div>
-          <div className="cart-item-actions">
-            {/* Buttons */}
-            <img
-              alt="increase"
-              className="action-icons"
-              src="https://as2.ftcdn.net/v2/jpg/01/07/62/07/1000_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg"
-              onClick={() => this.props.onIncreaseQuantity(this.props.product)}
-            />
-            <img
-              alt="decrease"
-              className="action-icons"
-              src="https://as2.ftcdn.net/v2/jpg/02/78/84/57/1000_F_278845758_9xl3srVgd8p4jquxgxugGaHV1e5EOlLO.jpg"
-              onClick={() => this.props.onDecreaseQuantity(this.props.product)}
-            />
-            <img
-              alt="delete"
-              className="action-icons"
-              src="https://as1.ftcdn.net/v2/jpg/03/40/32/90/1000_F_340329038_j7H8dA1F0vdbw4ltVYNdZe7b8zv1KWLu.jpg"
-              onClick={() => this.props.onDeleteProduct(this.props.product)}
-            />
-          </div>
+  const { price, title, qty } = props.product;
+  const { product, onIncreaseQuantity, onDecreaseQuantity, onDeleteProduct } =
+    props;
+
+  return (
+    <div className="cart-item">
+      <div className="left-block">
+        <img style={styles.image} alt="" src={product.img} />
+      </div>
+      <div className="right-block">
+        <div style={{ fontSize: 25 }}>{title}</div>
+        <div style={{ color: '#777' }}>Rs {price} </div>
+        <div style={{ color: '#777' }}>Qty: {qty} </div>
+        <div className="cart-item-actions">
+          {/* Buttons */}
+          <img
+            alt="increase"
+            className="action-icons"
+            src="https://as2.ftcdn.net/v2/jpg/01/07/62/07/1000_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg"
+            onClick={() => onIncreaseQuantity(product)}
+          />
+          <img
+            alt="decrease"
+            className="action-icons"
+            src="https://as2.ftcdn.net/v2/jpg/02/78/84/57/1000_F_278845758_9xl3srVgd8p4jquxgxugGaHV1e5EOlLO.jpg"
+            onClick={() => onDecreaseQuantity(product)}
+          />
+          <img
+            alt="delete"
+            className="action-icons"
+            src="https://as1.ftcdn.net/v2/jpg/03/40/32/90/1000_F_340329038_j7H8dA1F0vdbw4ltVYNdZe7b8zv1KWLu.jpg"
+            onClick={() => onDeleteProduct(product)}
+          />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 const styles = {
   image: {
